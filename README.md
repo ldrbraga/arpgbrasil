e6fff9747812201aecb4501024f444549d583244
Leandro Braga <ldrbraga@gmail.com>
2024-11-19 14:48:49 +0000

    add lint:check and lint:fix scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de clone para treinar algumas tecnologias
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Quando caminhamos de mãos dadas, chegamos juntos! ❤️</h1>
+  return <h1>Quando caminhamos de mãos dadas, chegamos juntos! ❤️</h1>;
 }
 
 export default Home;
