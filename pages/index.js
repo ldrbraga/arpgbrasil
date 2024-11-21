495b79a72d998f2ed6ff75d4effac49b161a59eb
Leandro Braga <ldrbraga@gmail.com>
2024-11-21 19:01:56 +0000

    feat: change text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-  return <h1>Quando caminhamos de mãos dadas, chegamos juntos! ❤️</h1>;
+  return (
+    <>
+      <h1>Em breve, aRPG Brasil!</h1>
+      <h3>
+        A maior comunidade brasileira dedicada aos fãs de action RPG. Encontre
+        builds otimizadas, dicas exclusivas e compartilhe sua paixão pelos
+        melhores jogos do gênero. Junte-se a nós e torne-se parte dessa jornada
+        épica!
+      </h3>
+    </>
+  );
 }
 
 export default Home;
